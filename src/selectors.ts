0bdfdab80fe4325b41c1dc8cb1ce1cae14cb8224
Jason Comes <jasonwcomes@gmail.com>
2022-10-12 10:27:09 -0500

    postfix classes with `pio` and use bem css styles

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,3 +1,3 @@
-export const rootElementId = "prismatic-io-embedded-app";
-export const modalSelector = `#${rootElementId} > .modal`;
-export const iframeContainerSelector = `#${rootElementId} .iframe-container`;
+export const rootElementId = "pio__embedded-app";
+export const modalSelector = `#${rootElementId} > .pio__modal`;
+export const iframeContainerSelector = `#${rootElementId} .pio__iframe`;
